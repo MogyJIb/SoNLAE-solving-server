6fd477cf7a5269ef74741eb7c2bf76f3ac557f01
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2019-01-03 17:06:12 +0300

    Add some logic

---
+++ SoNLAE-solving-server/CalculationLogic.cs
@@ -0,0 +1,52 @@
+﻿using SoNLAE_solving_server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoNLAE_solving_server
+{
+    class CalculationLogic
+    {
+        private int startLineIndex;
+        private int endLineIndex;
+
+        private int addVectorIndex;
+        private VectorInterface<Double>[] vectors;
+
+        private RestDTO restDTO;
+
+        public CalculationLogic(RestDTO restDTO)
+        {
+            this.restDTO = restDTO;
+
+            this.startLineIndex = restDTO.StartLineIndex;
+            this.endLineIndex = restDTO.EndLineIndex;
+            this.addVectorIndex = restDTO.AddVectorIndex;
+            this.vectors = restDTO.Vectors;
+
+        }
+
+        public RestDTO Run()
+        {
+            int i = addVectorIndex;
+            Double diagonalElement = vectors[i][i];
+
+            if (Double.IsInfinity(1 / diagonalElement)
+                    || Double.IsNaN(1 / diagonalElement))
+                return restDTO;
+
+            for (int j = startLineIndex; j < endLineIndex && j < vectors.Length; j++)
+            {
+                VectorInterface<Double> lineToAdd = vectors[i].Copy();
+                Double coefficient = -vectors[j][i] / diagonalElement;
+
+                lineToAdd.Mul(coefficient);
+                vectors[j].Add(lineToAdd);
+            }
+
+            return restDTO;
+        }
+    }
+}
--- SoNLAE-solving-server/Program.cs
+++ SoNLAE-solving-server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoNLAE_solving_server.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,25 +14,34 @@ namespace SoNLAE_solving_server
         static void Main(string[] args)
         {
             HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://127.0.0.1/");
+            if (args != null && args.Length > 0)
+                listener.Prefixes.Add(args[0]);
+            else
+                listener.Prefixes.Add("http://127.0.0.1/");
+
             listener.Start();
             Console.WriteLine("Ожидание подключений...");
 
             while (true)
             {
-                HttpListenerContext context = listener.GetContext();
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
-                response.ContentType = "application/json";
+                try
+                {
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+                    response.ContentType = "application/json";
 
-                string result = new StreamReader(request.InputStream).ReadToEnd();
+                    string result = new StreamReader(request.InputStream).ReadToEnd();
+                    RestDTO requestDTO = RestDTO.Deserialize(result);
 
-                string responseString = result;
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                response.ContentLength64 = buffer.Length;
-                Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+                    string responseString = RestDTO.Serialize(new CalculationLogic(requestDTO).Run());
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    response.ContentLength64 = buffer.Length;
+                    Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    output.Close();
+                }
+                catch(Exception exc) {  }
             }
         }
     }
